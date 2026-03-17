77eaba90645872fb0c75f06cf87d3dee8be5072c
JanzenWill <willjanzen@gustavus.edu>
2026-03-17 15:05:53 -0500

    gravity off, spawn direction, layers set

--- main.gd
+++ main.gd
@@ -14,15 +14,11 @@ func _on_mob_timer_timeout():
 	# Set the mob's position to the random location.
 	mob.position = mob_spawn_location.position
 
-	# Set the mob's direction perpendicular to the path direction.
-	var direction = mob_spawn_location.rotation + PI / 2
-
-	# Add some randomness to the direction.
-	direction += randf_range(-PI / 4, PI / 4)
-	mob.rotation = direction
+#hopefully sets the mob direction either right or left	
+	var direction = [0, PI][randi() % 2]
 
 	# Choose the velocity for the mob.
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	var velocity = Vector2(160, 0.0)
 	mob.linear_velocity = velocity.rotated(direction)
 
 	# Spawn the mob by adding it to the Main scene.
