6da8681c045ea4a056d1b6141ef3adcfb6c7a07b
JanzenWill <willjanzen@gustavus.edu>
2026-03-31 14:05:59 -0500

    Revert "began switch to fix dif speeds - cur broken"
    This reverts commit f7899872b381dc9521c8b2bdd4f177686db56abd.

---
+++ malicious_mob.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+@export var gravity = 0
+var velocity = Vector2.ZERO
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+"""
+func _process(delta: float) -> void:
+	velocity.y += gravity * delta
+	position += velocity * delta
+"""
+
+func _on_mob_timer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
