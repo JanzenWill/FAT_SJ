1dc503a780cd51130b9ae67bdac6d1886756cbd9
Will Janzen <willjanzen@gustavus.edu>
2026-04-09 15:43:21 -0500

    added evil mob spawning

--- evil_mob.gd
+++ evil_mob.gd
@@ -1,15 +1,15 @@
 extends RigidBody2D
 
 @export var gravity = 0
-@export var max_health = 2 #health
-@export var damage = 1 #damage it gives
+@export var max_health = 5 #health
+@export var damage = 2 #damage it gives
 
-var speed = 200
+var speed = 300
 var direction = 1
 var health = max_health
 var velocity = Vector2.ZERO
 
-signal killed
+#signal killed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -33,13 +33,16 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 func _physics_process(delta):
 	$AnimatedSprite2D.flip_h = linear_velocity.x < 0 #flips sprite
 
+'''
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("spear"):
 		if area.has_method("get_damage"):
 			take_damage(area.get_damage())
 		else:
 			take_damage(1)
-		
+'''
+
+'''	
 func take_damage(amount: int) -> void:
 	health -= amount
 	if health <= 0:
@@ -51,3 +54,4 @@ func die() -> void:
 
 func get_damage() -> int:
 	return damage
+'''
--- main.gd
+++ main.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var passive_mob_scene: PackedScene
 @export var malicious_mob_scene: PackedScene
+@export var evil_mob_scene: PackedScene
 
 var score = 0
 var depth_level = 0
@@ -47,23 +48,27 @@ func _on_passive_mob_timer_timeout():
 func check_depth() -> void:
 	var player_y = $Player.global_position.y
 	if player_y > depth_thresholds[depth_level] and depth_level < (depth_thresholds.size()-1):
-		print(player_y, "difficulty increase")
 		depth_level += 1
 		increase_difficulty()
+		
 	elif player_y < depth_thresholds[depth_level - 1] and depth_level > 0:
-		print(player_y, "difficulty_decrease")
 		depth_level -= 1
 		decrease_difficulty()
+		
 		
 func increase_difficulty() -> void:
 	$MaliciousMobTimer.wait_time *= 0.6
 	$PassiveMobTimer.wait_time *= 1.2
 	$Player.speed *= 0.8
+	$EvilMobTimer.start()
+	#print("difficulty increase ", depth_level)
 	
 func decrease_difficulty() -> void:
 	$MaliciousMobTimer.wait_time  *= 1.667
 	$PassiveMobTimer.wait_time *= 0.8334
 	$Player.speed *= 1.25
+	$EvilMobTimer.stop()
+	#print("difficulty decrease ", depth_level)
 	
 
 func _on_malicious_mob_timer_timeout() -> void:
@@ -127,4 +132,27 @@ func clear_all_fish() -> void: #clears all fish
 
 
 func _on_evil_mob_timer_timeout() -> void:
-	pass # Replace with function body.
+	#print("evilmobtimer timeout")
+	if depth_level < 1:
+		pass
+	else:
+		#print("Evil Mob Spawn Activated!")
+		var evil_mob = evil_mob_scene.instantiate()
+		evil_mob.add_to_group("fish") #added to group of fish that are removed with restart
+		
+		#evil_mob.killed.connect(_on_killed_malicious_mob)
+
+		var camera = $Player/Camera2D
+		#var viewport_size = get_viewport().size
+		
+		var spawn_x = camera.global_position.x + (700*[-1, 1].pick_random())
+		var spawn_y = camera.global_position.y + (100 * [-1, 1].pick_random())
+		
+
+		# Set the mob's position to the random location.
+		evil_mob.position = Vector2(spawn_x, spawn_y)
+		evil_mob.direction = [-1, 1].pick_random()
+
+		# Spawn the mob by adding it to the Main scene.
+		add_child(evil_mob)
+		
