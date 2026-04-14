db36faec12213603c2cdab81dd2ca73500b939be
Will Janzen <willjanzen@gustavus.edu>
2026-04-14 14:55:46 -0500

    Reapply "fix mob spawn and despawn"
    This reverts commit 8454ff9f4374816f07994e7146b165b7278dde47.

--- malicious_mob.gd
+++ malicious_mob.gd
@@ -1,24 +1,29 @@
 extends RigidBody2D
 
 @export var gravity = 0
-var velocity = Vector2.ZERO
+@export var base_velocity = 180
+@export var velocity_variability = 50
+@export var max_distance := 1000.0
+var player
+
+signal killed
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	player = get_tree().get_first_node_in_group("player")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-"""
 func _process(delta: float) -> void:
-	velocity.y += gravity * delta
-	position += velocity * delta
-"""
+	if player:
+		if global_position.distance_to(player.global_position) > max_distance:
+			queue_free()
+
 
 func _on_mob_timer_timeout() -> void:
 	pass # Replace with function body.
 
-
+"""
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+"""
