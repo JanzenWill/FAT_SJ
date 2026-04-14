8454ff9f4374816f07994e7146b165b7278dde47
Will Janzen <willjanzen@gustavus.edu>
2026-04-14 14:51:04 -0500

    Revert "fix mob spawn and despawn"
    This reverts commit c2e05543422a5ad9a2c681e7585eff321dd195df.

--- malicious_mob.gd
+++ malicious_mob.gd
@@ -1,29 +1,24 @@
 extends RigidBody2D
 
 @export var gravity = 0
-@export var base_velocity = 180
-@export var velocity_variability = 50
-@export var max_distance := 1000.0
-var player
-
-signal killed
+var velocity = Vector2.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player = get_tree().get_first_node_in_group("player")
+	pass # Replace with function body.
 
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+"""
 func _process(delta: float) -> void:
-	if player:
-		if global_position.distance_to(player.global_position) > max_distance:
-			queue_free()
-
+	velocity.y += gravity * delta
+	position += velocity * delta
+"""
 
 func _on_mob_timer_timeout() -> void:
 	pass # Replace with function body.
 
-"""
+
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
-"""
--- passive_mob.gd
+++ passive_mob.gd
@@ -1,29 +1,30 @@
 extends RigidBody2D
 
 @export var gravity = 0
-@export var base_velocity = 180
-@export var velocity_variability = 50
-@export var max_distance := 1000.0
-var player
+var velocity = Vector2.ZERO
 
 signal killed
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	player = get_tree().get_first_node_in_group("player")
+func _ready() -> void:
+	pass # Replace with function body.
 
-func _process(delta):
-	if player:
-		if global_position.distance_to(player.global_position) > max_distance:
-			queue_free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	velocity.y += gravity * delta
+#	position += velocity * delta
+
+
+func _on_mob_timer_timeout() -> void:
+	pass # Replace with function body.
+
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("spear"):
 		killed.emit()
 		queue_free()
-		
-		
-"""
+
+
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void: #makes them disapear off screen dont know if we want that
 	queue_free() # Replace with function body.
-"""
